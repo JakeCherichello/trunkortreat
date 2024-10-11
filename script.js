5b1e711c8f57ab130b0edbffa6e9678f5b78bead
JakeCherichello <Cherichello1104@icloud.com>
2024-10-11 09:26:00 -0400

    finished front page and Jquery

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+    // Home page
+    $('#moreInfoBtn').on('click', function() {
+        $('#eventDescription').toggle();
+    });
 
     // Trunk showcase page
     $('.trunk-container').on('mouseenter', function() {
@@ -7,5 +11,5 @@ $(document).ready(function() {
         $(this).find('.trunk-description').css('transform', 'translateY(100%)');
     });
 
-   
+    
 });
