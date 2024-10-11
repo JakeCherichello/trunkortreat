4154afc1bd9b4ff15c99b9e7295416a248f05915
JakeCherichello <Cherichello1104@icloud.com>
2024-10-11 08:36:33 -0400

    did trunk showcase

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
-    // Home page
-    $('#moreInfoBtn').on('click', function() {
-        $('#eventDescription').toggle();
-    });
-
+   =
     // Trunk showcase page
     $('.trunk-container').on('mouseenter', function() {
         $(this).find('.trunk-description').css('transform', 'translateY(0)');
@@ -11,8 +7,5 @@ $(document).ready(function() {
         $(this).find('.trunk-description').css('transform', 'translateY(100%)');
     });
 
-    // Schedule page
-    $('.event-heading').on('click', function() {
-        $(this).next('.event-details').slideToggle();
-    });
+
 });
