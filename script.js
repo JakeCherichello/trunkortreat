67768559af11873b03425bbccfaba260a16f9de1
JakeCherichello <Cherichello1104@icloud.com>
2024-10-15 10:07:41 -0400

    finished i think

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ $(document).ready(function() {
     $('#moreInfoBtn').on('click', function() {
         $('#eventDescription').toggle();
     });
-
     // Trunk showcase page
     $('.trunk-container').on('mouseenter', function() {
         $(this).find('.trunk-description').css('transform', 'translateY(0)');
     }).on('mouseleave', function() {
         $(this).find('.trunk-description').css('transform', 'translateY(100%)');
     });
-
-    
+    // Schedule page
+    $('.event-heading').on('click', function() {
+        $(this).next('.event-details').slideToggle();
+    });
 });
